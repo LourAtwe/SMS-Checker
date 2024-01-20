36da772a58dd9145177a0d0725ec9ab7f663881d
LourAtwe <109828253+LourAtwe@users.noreply.github.com>
2024-01-20 23:03:58 +0300

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,41 +1,49 @@
-from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from nltk.tokenize import word_tokenize
-from sklearn.metrics import accuracy_score,confusion_matrix,precision_score,recall_score,f1_score
+import tkinter
+import sys
+from tkinter import *
+from tkinter import Label, PhotoImage
 import joblib
+import os
 
+lour = tkinter.Tk()
+lour.geometry("900x650")
+lour.title('SMS SPAM')
+bg = PhotoImage(file ='22.png')#Enter your picutre path that is already in the project folder.
+label1 = Label(lour, image = bg)
+label1.place(x = 0, y = 0)
 
-df = pd.read_csv('spam.csv',encoding='latin1')
-X = df['v2']
-Y = df['v1']
+entry_font = ("Comic Sans MS", 40)
+loaded_model = joblib.load('finalized_model.sav')
 
-def tokenize_text(text):
-    tokens = word_tokenize(text)
-    return tokens
+comic_sans_font = ("Comic Sans MS", 20)
+comic_sans_font1 = ("Comic Sans MS", 17)
+comic_sans_font2 = ("Comic Sans MS", 20)
 
-df['v2'] = df['v2'].apply(tokenize_text)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
+def submit_text():
+    entered_text = [str(entry.get())]
+    loaded_vectorizer = joblib.load('vectorizer.joblib')
+    new_data_transformed = loaded_vectorizer.transform(entered_text)
+    predictions1 = loaded_model.predict(new_data_transformed)
+    if predictions1[0]=="ham":
+         result_label.config(text=f"(Your Text is: HAM \U0001F389 😊 ) ")
+    else:
+        result_label.config(text=f"(Your Text is: SPAM \U00002620 😞 )")
 
-# Create and fit the model
-vectorizer = TfidfVectorizer(lowercase=False)
-X_train_transformed = vectorizer.fit_transform(X_train)
-model = DecisionTreeClassifier()
-model.fit(X_train_transformed, Y_train)
+label = tkinter.Label(lour, text="Enter your text here! :",bd=3, font=comic_sans_font2)
+label.grid( column=1, padx= 100, pady= 30)
 
-# Evaluate the model
-X_test_transformed = vectorizer.transform(X_test)
-predictions = model.predict(X_test_transformed)
-accuracy = accuracy_score(Y_test, predictions)
-print(f"Accuracy on the test dataset: {accuracy * 100}")
-filename = 'finalized_model.sav'
-joblib.dump(model, filename)
-joblib.dump(vectorizer, 'vectorizer.joblib')
+entry = tkinter.Entry(lour, width=50, bd=3, font=comic_sans_font2)
+entry.grid( column=1, padx= 40, pady= 20)
+#entry.pack(pady=20)
 
+button = tkinter.Button(lour, text='Check the SMS if it is SPAM or NOT', width=30,bd=3, command=submit_text,font=comic_sans_font1,bg="pink",anchor="w")
+#button.pack(pady=30)
+button.grid( column=1, padx= 40, pady= 20)
 
-new_data = [str(input("Enter your SMS: "))]
-new_data_transformed = vectorizer.transform(new_data)
-predictions = model.predict(new_data_transformed)
-print("your Text is :",predictions)
+result_label = tkinter.Label(lour,text="",fg="white",bg="orange",font=comic_sans_font)
+#result_label.pack()
+result_label.grid( column=1, padx= 40, pady= 20)
+
+lour.mainloop()
+#Check the SMS if it is SPAM or NOT
 
